340761dd7f970a3cb30df31542c6a2b9f84d1222
Nikita Avstriyskiy <smileneverdie@gmail.com>
2022-10-27 18:56:22 +0300

    фÑСделали скетдллет ÑÑфронта

--- database/migrations/2022_10_13_204305_create_users_table.php
+++ database/migrations/2022_10_13_204305_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('password');
             $table->string('email');
+            $table->string('remember_token')->nullable();
             $table->timestamps();
         });
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <a href="#" class="btn btn-dark">Создать задачу</a>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+<div class="row row-cols-3" style="margin-left: 3rem">
+    <div class="col-3" style="text-align: center">
+        <h3>Новое</h3>
+        <div class="card">
+            <a href="#" class="btn btn-outline-dark">Поменять статус</a>
+            <div class="card-body">
+                СДЕЛАТЬ КАКАШКИ
+            </div>
+        </div>
+    </div>
+    <div class="col-3 " style="text-align: center">
+        <h3>В работе</h3>
+        <div class="card">
+            <a href="#" class="btn btn-outline-dark">Поменять статус</a>
+            <div class="card-body">
+                СДЕЛАТЬ КАКАШКИ
+            </div>
+        </div>
+        <div class="card">
+            <a href="#" class="btn btn-outline-dark">Поменять статус</a>
+            <div class="card-body">
+                СДЕЛАТЬ ПОНОС
+            </div>
+        </div>
+    </div>
+    <div class="col-3" style="text-align: center">
+        <h3>На проверке</h3>
+        <div class="card">
+            <a href="#" class="btn btn-outline-dark">Поменять статус</a>
+            <div class="card-body">
+                СДЕЛАТЬ КАКАШКИ
+            </div>
+        </div>
+    </div>
+    <div class="col-3 align-content-center" style="text-align: center">
+        <h3>Готово</h3>
+        <div class="card">
+            <a href="#" class="btn btn-outline-dark">Поменять статус</a>
+            <div class="card-body">
+                СДЕЛАТЬ КАКАШКИ
             </div>
         </div>
     </div>
